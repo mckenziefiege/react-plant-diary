89d15039dfde4f22176e1294a4e732233bea9e3d
McKenzie Fiege <mckenziefiege@McKenzies-MBP.home>
2019-03-05 16:09:38 -0500

    added login and signup form and functions to handle login and signup in app.js

--- src/App.js
+++ src/App.js
@@ -12,6 +12,79 @@ import ToDoList from './Components/ToDoList.js'
 import Task from './Components/Task.js'
 
 class App extends Component {
+  state = {
+  auth: { currentUser: {} },
+}
+
+componentDidMount () {
+  const token = localStorage.getItem('token')
+  if (token) {
+    fetch('http://localhost:3000/profile', {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    })
+    .then(resp => resp.json())
+    .then(resp =>
+      this.setState({
+        auth: {currentUser: resp.user}
+      })
+    )
+  }
+}
+
+handleLogout = () => {
+  localStorage.removeItem("token")
+  this.setState({ auth: { currentUser: {} } })
+  this.props.history.push("/")
+}
+
+handleLogin = (e) => {
+   e.preventDefault()
+   fetch("http://localhost:3000/login", {
+     method: "POST",
+     headers: {
+       "Content-Type": "application/json",
+       Accept: "application/json"
+     },
+     body: JSON.stringify({
+       user: {
+         username: e.target.username.value,
+         password: e.target.password.value
+       }
+     })
+   })
+     .then(resp => resp.json())
+     .then(resp => {
+       localStorage.setItem("token", resp.jwt)
+       this.setState({
+         auth: {currentUser: resp.user}
+       })
+     })
+ }
+
+ handleSignup = (e) => {
+   e.preventDefault()
+   const options = {
+     method: 'POST',
+     headers: {'Content-Type': 'application/json'},
+     body: JSON.stringify({ user: {
+       first_name: e.target['first-name'].value,
+       username: e.target.username.value,
+       password: e.target.password.value,
+       image: e.target.image.value }
+     })
+   }
+   fetch('http://localhost:3000/users', options)
+     .then(resp => resp.json())
+     .then(resp => {
+       localStorage.setItem("token", resp.jwt)
+       this.setState({
+         auth: {currentUser: resp.user}
+       })
+     })
+ }
+
   render() {
     return (
       <div>
